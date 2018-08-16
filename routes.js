a472c9f18023975dd656d9814e6dd1c5894a5826
Nat <nat_sirisamphan@hotmail.com>
2018-08-17 10:16:20 +1200

    set up docs, server running

---
+++ routes.js
@@ -0,0 +1,11 @@
+let express = require('express')
+let router = express.Router()
+const data = require('./produce.json')
+// let fs = require('fs')
+//let empty = {}
+module.exports = router
+
+router.get ('/', (req, res) => {
+  res.send('Hello world')
+})
+
